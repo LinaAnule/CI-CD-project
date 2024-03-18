a416f3db240cdc3b3f98a224141d2093d208de51
Lina <lina.anule@gmail.com>
2024-03-19 01:49:58 +0200

    nothing commit 4

--- src/test/java/it/academy/message/MessageControllerTest.java
+++ src/test/java/it/academy/message/MessageControllerTest.java
@@ -30,7 +30,7 @@ class MessageControllerTest {
 		Mockito.when(messageBean.getMessage()).thenReturn(message);
 
 		String result = controller.createMessage(message);
-		//nothing here 3
+		//nothing here 4
 		Assertions.assertThat(result).isEqualTo("Message: `" + message + "` was save successfully.");
 	}
 
